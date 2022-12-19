8ffc45dbd9ba3a43fe7023df380950aee1e3e93e
Isuru Fernando <isuruf@gmail.com>
2022-12-18 23:33:04 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ if [[ "$target_platform" == "win-64" ]]; then
   export ac_cv_have_decl__BitScanForward=yes
   export build_alias=x86_64-pc-mingw64
   export host_alias=x86_64-pc-mingw64
+  export CC_FOR_BUILD=$CC
   autoreconf -iv
   ./configure --prefix=$PREFIX --with-int=$val_int_type --disable-shared CFLAGS="$CFLAGS" || (cat config.log && false)
   patch_libtool
